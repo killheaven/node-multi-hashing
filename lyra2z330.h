3b7590ad8f96e7c7402c39950150ea8a6424e869
killheaven <killheaven@gmail.com>
2017-10-16 21:30:28 +0800

    update

--- lyra2z330.h
+++ lyra2z330.h
@@ -2,15 +2,17 @@
 #define LYRA2Z330_H
 
 #ifdef __cplusplus
+
 extern "C" {
+
 #endif
 
-#include <stdint.h>
-
-void lyra2z330_hash(const char* input, char* output);
+void lyra2z330_hash(const char* input, char* output, uint32_t len);
 
 #ifdef __cplusplus
+
 }
+
 #endif
 
 #endif
